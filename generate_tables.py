d32229ff0239b91942d5a78b4a536f3e49407b77
Louis-XIV-bis <tigerwhite27@hotmail.fr>
2024-04-16 15:47:24 +0200

    should work

--- generate_tables.py
+++ generate_tables.py
@@ -8,24 +8,38 @@ import sys, os
 sys.path.append("workflow/scripts/")
 from functions import *
 
-def main():
+def main(method):
     
+    # Read the config file
+    with open("config/config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    
+    # Define the list of ID and the result path according to the method passed in 
+    # command-line (get or merge_gvcf)
+    valid_options = ["get_gvcf", "merge_gvcf"]
+    
+    # Check if the argument is valid
+    if method in valid_options:
+        if method == "get_gvcf":
+            ENA = config["ENA_ID_get_gvcf"]  # list of ENA IDs
+            results_dir = "./results/tables_get_gvcf/"
+
+        elif method == "merge_gvcf":
+            ENA = config["ENA_ID_merge_gvcf"]  # list of ENA IDs
+            results_dir = "./results/tables_merge_gvcf/"
+    else: 
+        raise ValueError("Invalid argument. Please provide 'get_gvcf' or 'merge_gvcf'.")
+
+    tax_id = config["tax_id"]  # taxon id is specific for each species
+
     ############# Downloading, merging and filtering the information files #############
     # Download all the tsv files from the ENA IDs. They contain all the information
     # requiered for the rest of the pipeline (sample ID, ftp link to dl fastq, etc).
     # After that, it's merged into one unique file and filtered / processed (e.g. keep
     # only S.cere sequences). Then, a csv table is created for each ENA_strain ID from the table.
-    
-    with open("config/config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-    
-    ENA = config["ENA_ID_get_gvcf"]  # list of ENA IDs
-    tax_id = config["tax_id"]  # taxon id is specific for each species
 
     # Check if the output directory exists, if not, create it otherwise 
-    # remove the existing file to avoid conflict 
-    results_dir = "./results/table/"
-
+    # remove the existing file to avoid conflict    
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
     else: 
@@ -42,12 +56,15 @@ def main():
     # Split the table to reate the files for each ENA_strain and the associated list to use for the pipeline
     ENA_strain_list = split_and_save_csv(results_dir, "merged_filtered_table.csv", )
 
-    # # Check if the pipeline already ran (and produced a list of ENA_strain IDs), if yes, 
-    # # add the existing list to the new list in order to have the correct input for the pipeline (all strain_IDs)
+    # Save the ENA_strain list to a file (will be the names of the resulting files)
     ENA_strain_path = results_dir + "ENA_strain_list.json"
     with open(ENA_strain_path, 'w') as file:
         ENA_strain_list = ENA_strain_list.tolist()
         json.dump(ENA_strain_list, file)
 
 if __name__ == "__main__":
-    main()
+    # Check if an argument is provided
+    if len(sys.argv) < 2:
+        print("Usage: python script.py get_gvcf/merge_gvcf")
+    else:
+        main(sys.argv[1])
